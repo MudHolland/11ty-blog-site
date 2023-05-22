80fdcb1edbb42c112e1975f16af0503af13ceedc
MudHolland <103423480+MudHolland@users.noreply.github.com>
2023-05-22 20:36:01 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 const { DateTime } = require("luxon");
+const markdownIt = require('markdown-it');
+const md = markdownIt();
 
 module.exports = function(eleventyConfig) {
 
@@ -10,26 +12,8 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./src/assets');
 	eleventyConfig.addPassthroughCopy('./src/admin');
 
-	eleventyConfig.addFilter("postDate", (dateObj) => {
-		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-})
-
-	return {
-		dir: {
-			input: "src",
-			output: "public"
-		}
-	}
-}
-
-const markdownIt = require('markdown-it');
-
-module.exports = function (eleventyConfig) {
-  // Create a new MarkdownIt instance
-  const md = markdownIt();
-
-  // Override the default image rendering rule
-  md.renderer.rules.image = function (tokens, idx, options, env, self) {
+// Override the default image rendering rule
+md.renderer.rules.image = function (tokens, idx, options, env, self) {
     // Get the image details from the token
     const src = tokens[idx].attrs[0][1];
     const alt = tokens[idx].attrs[1][1];
@@ -43,4 +27,17 @@ module.exports = function (eleventyConfig) {
 
   // Add the modified MarkdownIt instance to Eleventy's markdown library
   eleventyConfig.setLibrary('md', md);
+
+
+
+	eleventyConfig.addFilter("postDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+	})
+
+	return {
+		dir: {
+			input: "src",
+			output: "public"
+		}
+	}
 };
