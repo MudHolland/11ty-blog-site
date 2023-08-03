b2b92ea9dfaa42aece69abac1e207e71ee8fce8d
MudHolland <103423480+MudHolland@users.noreply.github.com>
2023-08-03 10:33:02 +0200

    Update modal.js

--- src/modal.js
+++ src/modal.js
@@ -1,26 +1,67 @@
-window.onload = function(){
-	// Get the modal, get the image and insert it inside the modal - use its "alt" text as a caption
-	var modal = document.getElementById('myModal');
-	var img = $('img');
-	var modalImg = $("#img01");
-	var captionText = document.getElementById("caption");
-
-	$('img').click(function(){
+(function () {
+	function openModal(img) {
+	  if (modal) {
 		modal.style.display = "block";
-		var newSrc = this.src;
-		modalImg.attr('src', newSrc);
-		captionText.innerHTML = this.alt;
+		modalImg.src = img.src;
+		captionText.innerHTML = img.alt;
+	  }
+	}
+  
+	const images = document.querySelectorAll('img');
+	images.forEach(function (img) {
+	  img.addEventListener('click', function () {
+		openModal(this);
+	  });
 	});
-
-	// Get the elements that close the modal and listen for clicks
-	var span = document.getElementsByClassName("close")[0];
-	var screen = document.getElementsByClassName("modal")[0];
-
-	span.onclick = function() {
+  
+	function closeModal() {
+	  if (modal) {
 		modal.style.display = "none";
+	  }
 	}
-	
-	screen.onclick = function() {
-		modal.style.display = "none";
+  
+	const modal = document.getElementById('myModal');
+	const modalImg = modal ? document.getElementById("img01") : null;
+	const captionText = modal ? document.getElementById("caption") : null;
+  
+	const span = document.getElementsByClassName("close")[0];
+	const screen = document.getElementsByClassName("modal")[0];
+  
+	if (span) {
+	  span.onclick = closeModal;
 	}
-}
+  
+	if (screen) {
+	  screen.onclick = closeModal;
+	}
+  
+	function loadImage(img) {
+	  return new Promise((resolve) => {
+		if (img.complete) {
+		  resolve();
+		} else {
+		  img.addEventListener('load', resolve);
+		}
+	  });
+	}
+  
+	async function handleImageAspectRatio(figureElement) {
+	  const imgElement = figureElement.querySelector('img');
+	  await loadImage(imgElement);
+  
+	  const image = new Image();
+	  image.src = imgElement.src;
+  
+	  if (image.height > image.width) {
+		figureElement.classList.add('half-width');
+	  }
+	}
+  
+	document.addEventListener("DOMContentLoaded", function () {
+	  const figureElements = document.querySelectorAll('figure');
+	  figureElements.forEach(function (figureElement) {
+		handleImageAspectRatio(figureElement);
+	  });
+	});
+  })();
+  
